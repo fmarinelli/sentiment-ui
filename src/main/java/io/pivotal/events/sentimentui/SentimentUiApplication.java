7c11efa0ed04ee188d7b726bf72191fbc0d51f52
Fabio Marinelli <f.marinelli@gmail.com>
2018-01-29 16:40:32 +0100

    first commit

---
+++ src/main/java/io/pivotal/events/sentimentui/SentimentUiApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.events.sentimentui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentUiApplication.class, args);
+	}
+}
---
+++ src/test/java/io/pivotal/events/sentimentui/SentimentUiApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal.events.sentimentui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SentimentUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
